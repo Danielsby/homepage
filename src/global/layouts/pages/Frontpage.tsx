1df4956f1ea864d61539bbc5b533c687669c4067
Daniel <danielsby@outlook.com>
2019-09-05 23:18:05 +0200

    Frontend improvements.

--- src/global/layouts/pages/Frontpage.tsx
+++ src/global/layouts/pages/Frontpage.tsx
@@ -9,8 +9,7 @@ export const Frontpage: React.FC = () => {
             </header>
 
             <section className='status'>
-                <h6>Webpages: Under active development and might therefore be lacking for the time being.</h6>
-                <h6>Status: Open - Looking for a frontend developer position </h6>
+                <p>Status: Open - Looking for a frontend developer position </p>
             </section>
 
             <article className='weather'>
@@ -19,7 +18,7 @@ export const Frontpage: React.FC = () => {
 
             <main className='news'>
                 <article>
-                    Nimado projects are about to begin it's pre-production!
+                    <p>Nimado projects are about to begin it's pre-production!</p>
                 </article>
             </main>
         </div>
