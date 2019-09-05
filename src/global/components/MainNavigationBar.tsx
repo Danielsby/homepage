819c645f9267d15cc85b701f9e85972ec165c01a
Daniel <danielsby@outlook.com>
2019-09-05 22:15:44 +0200

    Second fix regarding url

--- src/global/components/MainNavigationBar.tsx
+++ src/global/components/MainNavigationBar.tsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 export const MainNavigationBar: React.FC = () => {
     return (
         <ul>
-            <li><NavLink to='/'>Home</NavLink></li>
+            <li><NavLink to='/homepage'>Home</NavLink></li>
             <li><NavLink to='/schedule'>Schedule</NavLink></li>
             <li><NavLink to='/projects'>Projects</NavLink></li>
             <li><NavLink to='/about'>About</NavLink></li>
