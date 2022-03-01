b602ca3b3d3c9e24501245441df912aa0d7d01ba
daniel.by <daniel.by@makingwaves.com>
2022-03-01 11:19:35 +0100

    refactor: more dynamic link code

---
+++ addLink.js
@@ -0,0 +1,9 @@
+function insertLink(id, url) {
+  const site = document.getElementById(id);
+  site.href = url;
+}
+
+// Add links
+insertLink('linkedin', 'https://www.linkedin.com/in/dbygfgfdfdf');
+insertLink('github', 'https://www.github.com/danielsby');
+
